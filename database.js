ca7333de1e1849ae737d4bed764c0495d74576ec
sadique12345 <88301944+sadique12345@users.noreply.github.com>
2026-05-01 20:54:37 +0530

    Update database.js

--- database.js
+++ database.js
@@ -5,9 +5,9 @@ export function createDatabase({ graphFile }) {
   const backend = process.env.GRAPH_BACKEND || "file";
 
   if (backend === "neo4j") {
-    const httpUrl = process.env.https://5eaa5b4d.neo4j.io/db/neo4j/tx/commit;
-    const username = process.env.5eaa5b4d;
-    const password = process.env.BosMVLmr3cradeCkxwmXg98gYZigoLUgGwvKa0iVAcQ;
+    const httpUrl = "https://5eaa5b4d.neo4j.io/db/neo4j/tx/commit";
+    const username = "5eaa5b4d";
+    const password = "BosMVLmr3cradeCkxwmXg98gYZigoLUgGwvKa0iVAcQ";
 
     if (!httpUrl || !username || !password) {
       throw new Error("Neo4j mode requires NEO4J_HTTP_URL, NEO4J_USERNAME, and NEO4J_PASSWORD.");
